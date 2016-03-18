6fdff3a79c2b0174b901c4ce65f93c92e16821dc
qyfei <qyfei2010@gmail.com>
2016-03-18 13:53:03 +0800

    Initial commit

---
+++ src/main/java/Shuffle1.java
@@ -0,0 +1,22 @@
+/**
+ * Created by qyfei on 2016-3-18.
+ */
+public class Shuffle1 {
+    public static void main(String [] args){
+int x =3;
+        while (x > 0){
+            if (x>2){
+                System.out.print("a");
+            }
+            x= x-1;
+            System.out.print("_");
+            if (x==2){
+                System.out.print("b c");
+            }
+            if (x==1){
+                System.out.print("d");
+                x=x-1;
+            }
+        }
+    }
+}
